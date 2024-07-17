ea29c5d5ca696d486f3082e9e26de5d2e33d8e92
Sara Choudhary <155305694+sarachoudhary@users.noreply.github.com>
2024-07-17 17:34:33 +0530

    Create valid-parentheses.java
    solution to problem of 20 leetcode

---
+++ valid-parentheses.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(' || s.charAt(i) =='[' || s.charAt(i)=='{'){
+                st.push(s.charAt(i));
+            } else {
+                if(!st.isEmpty()){
+                    if(s.charAt(i)==')' && st.peek()=='('){
+                        st.pop();
+                    } else if(s.charAt(i)==']' && st.peek()=='['){
+                        st.pop();
+                    }else if(s.charAt(i)=='}' && st.peek()=='{'){
+                        st.pop();
+                    }else{
+                        st.push(s.charAt(i));
+                    }
+                }else {
+                    st.push(s.charAt(i));
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
